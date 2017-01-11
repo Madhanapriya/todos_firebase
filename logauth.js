8781429c0876e552470f0998b666e9f099aa63fc
Madhanapriya <madhanapriya1986@gmail.com>
2017-01-11 20:01:47 +0530

    user

--- logauth.js
+++ logauth.js
@@ -1,8 +1,7 @@
 var config = {databaseURL: "https://authenticate-1d688.firebaseio.com/"};
 firebase.initializeApp(config);
 
-function auth()
-{  
+
 
 	var txtemail=document.getElementById('txtemail');
 	var txtpass=document.getElementById('txtpass');
@@ -16,19 +15,11 @@ btnLogin.addEventListener('click',function()
 	var email=txtemail.value;
 	var pass=txtpass.value;
 	var auth=firebase.auth();
+	
 	var promise=auth.signInWithEmailAndPassword(email, pass);
 	promise.catch(function(error)  {
-		var errorCode = error.code;
-      var errorMessage = error.message;
-     if (errorCode == 'auth/weak-password')
-	  {
-    alert('The password is too weak.');
-  } 
-  else 
-  {
-    alert(errorMessage);
-  }
-  console.log(error);});
+		
+  console.log(e.message);});
  });
   btnSignUp.addEventListener('click',function()
 { 
@@ -37,19 +28,8 @@ alert("ok");
 	var pass=txtpass.value;
 	var auth=firebase.auth();
 	var promise=auth.createUserWithEmailAndPassword(email, pass);
-	promise.catch(function(error)  
-	{
-		var errorCode = error.code;
-  var errorMessage = error.message;
-  if (errorCode == 'auth/weak-password')
-	  {
-    alert('The password is too weak.');
-  } else 
-  {
-    alert(errorMessage);
-  }
-  console.log(error);
-});
+	promise.catch(function(error) { 
+	  console.log(e.message);});
  });
 firebase.auth().onAuthStateChanged(firebaseuser,function()
  {
@@ -61,5 +41,4 @@ firebase.auth().onAuthStateChanged(firebaseuser,function()
   }
 });
  
-return false
-}
+
